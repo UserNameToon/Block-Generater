396f47956e9868c233f4370b332b0f2e6da18ee9
UserNameToon <chanchatri.c@gmail.com>
2019-03-04 15:03:52 -0700

    Add files via upload

---
+++ CMPE2300_ICA_09/ICA09_Toon/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ICA09_Toon
+{
+    partial class UI_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_NumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.UI_List_Butt = new System.Windows.Forms.Button();
+            this.UI_Link_Butt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_NumUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_NumUpDown
+            // 
+            this.UI_NumUpDown.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.UI_NumUpDown.Location = new System.Drawing.Point(13, 13);
+            this.UI_NumUpDown.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.UI_NumUpDown.Name = "UI_NumUpDown";
+            this.UI_NumUpDown.Size = new System.Drawing.Size(259, 20);
+            this.UI_NumUpDown.TabIndex = 0;
+            this.UI_NumUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.UI_NumUpDown.ValueChanged += new System.EventHandler(this.UI_NumUpDown_ValueChanged);
+            // 
+            // UI_List_Butt
+            // 
+            this.UI_List_Butt.Location = new System.Drawing.Point(13, 40);
+            this.UI_List_Butt.Name = "UI_List_Butt";
+            this.UI_List_Butt.Size = new System.Drawing.Size(259, 39);
+            this.UI_List_Butt.TabIndex = 1;
+            this.UI_List_Butt.Text = "Make List";
+            this.UI_List_Butt.UseVisualStyleBackColor = true;
+            this.UI_List_Butt.Click += new System.EventHandler(this.UI_List_Butt_Click);
+            // 
+            // UI_Link_Butt
+            // 
+            this.UI_Link_Butt.Location = new System.Drawing.Point(12, 85);
+            this.UI_Link_Butt.Name = "UI_Link_Butt";
+            this.UI_Link_Butt.Size = new System.Drawing.Size(259, 39);
+            this.UI_Link_Butt.TabIndex = 2;
+            this.UI_Link_Butt.Text = "Populate Linked List";
+            this.UI_Link_Butt.UseVisualStyleBackColor = true;
+            this.UI_Link_Butt.Click += new System.EventHandler(this.UI_Link_Butt_Click);
+            // 
+            // UI_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 137);
+            this.Controls.Add(this.UI_Link_Butt);
+            this.Controls.Add(this.UI_List_Butt);
+            this.Controls.Add(this.UI_NumUpDown);
+            this.Name = "UI_Form";
+            this.Text = "ICA09";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_NumUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown UI_NumUpDown;
+        private System.Windows.Forms.Button UI_List_Butt;
+        private System.Windows.Forms.Button UI_Link_Butt;
+    }
+}
+
---
+++ CMPE2300_ICA_09/ICA09_Toon/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ICA09_Toon
+{
+    public partial class UI_Form : Form
+    {
+        static Random rng = new Random();
+        List<Point> pointList = new List<Point>();
+        LinkedList<Point> pointLink = new LinkedList<Point>();
+        CDrawer canvas = new CDrawer();
+
+        public UI_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_List_Butt_Click(object sender, EventArgs e)
+        {
+            pointList.Clear();    
+            while (pointList.Count != ((canvas.ScaledWidth / (int)UI_NumUpDown.Value) * (canvas.ScaledHeight / (int)UI_NumUpDown.Value)))
+            {
+                Point newPoint = new Point(rng.Next(0, (canvas.ScaledWidth / (int)UI_NumUpDown.Value))* (int)UI_NumUpDown.Value,
+                                           rng.Next(0, (canvas.ScaledHeight / (int)UI_NumUpDown.Value) )* (int)UI_NumUpDown.Value);
+                if (pointList.Count == 0)
+                    pointList.Add(newPoint);
+                else
+                {
+                    if (!pointList.Contains(newPoint))
+                        pointList.Add(newPoint);
+                }
+            }
+            canvas.Clear();
+            for (int i = 0; i < pointList.Count - 1; i++)
+            {
+                canvas.AddLine(pointList[i].X, pointList[i].Y, pointList[i + 1].X, pointList[i + 1].Y, Color.Magenta);
+            }
+            canvas.Render();
+            Text = "Made N Points";
+            UI_List_Butt.Text = "List contains: " + pointList.Count + " points";
+        }
+
+        private void UI_Link_Butt_Click(object sender, EventArgs e)
+        {
+
+            pointLink.Clear();
+            foreach(Point i in pointList)
+            {
+                if (pointLink.Count == 0)
+                    pointLink.AddFirst(i);
+                else
+                {
+                    LinkedListNode<Point> nodePoint = pointLink.First;
+                    while(nodePoint.Next != null && (i.Y * canvas.ScaledWidth + i.X) >= (nodePoint.Value.Y * canvas.ScaledWidth + nodePoint.Value.X))
+                        nodePoint = nodePoint.Next;
+
+                    if (nodePoint.Next == null && (i.Y * canvas.ScaledWidth + i.X) >= (nodePoint.Value.Y * canvas.ScaledWidth + nodePoint.Value.X))
+                        pointLink.AddLast(i);
+                    else
+                        pointLink.AddBefore(nodePoint, i);      
+                }
+            }
+            canvas.Clear();
+
+            for (LinkedListNode<Point>  newNode = pointLink.First; newNode.Next != null; newNode = newNode.Next)
+            {
+                canvas.AddLine(newNode.Value.X, newNode.Value.Y, newNode.Next.Value.X, newNode.Next.Value.Y, Color.Yellow);
+            }
+            canvas.Render();
+            UI_Link_Butt.Text = "LinkedList contains: " + pointLink.Count + " points";
+        }
+
+        private void UI_NumUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            UI_List_Butt.Text = "Make list";
+            UI_Link_Butt.Text = "Populate Linked List";
+        }
+    }
+}
